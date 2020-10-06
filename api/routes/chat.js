1b9b4de747ce1cb763743d97b581e7f0ed6913dd
Abhinav Chawla <abhinav.chawla@ibm.com>
2020-10-06 18:10:52 -0400

    route

--- api/routes/chat.js
+++ api/routes/chat.js
@@ -2,23 +2,10 @@ let { Router } = require("express");
 let { celebrate, Joi } = require("celebrate");
 const route = Router();
 
-let { create, webhook } = require("../../controllers/chat");
+let { webhook } = require("../../controllers/chat");
 
 module.exports = (app) => {
   app.use("/chat", route);
-
-  // TODO: fix celebrate body
-  route.post(
-    "/",
-    // celebrate({
-    //   body: Joi.object({
-    //     coilId: Joi.string().required(),
-    //     provider: Joi.string().required(),
-    //     clientId: Joi.string().required(),
-    //   }),
-    // }),
-    [create]
-  );
 
   route.post("/webhook", [webhook]);
 };
--- controllers/chat.js
+++ controllers/chat.js
@@ -1,13 +1,8 @@
-async function create(req, res) {
-  return res.send({ status: "All Good!" });
-}
-
 async function webhook(req, res) {
   console.log(req.body);
   return res.send("All good");
 }
 
 module.exports = {
-  create,
   webhook,
 };

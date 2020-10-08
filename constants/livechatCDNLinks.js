7cb6ad8a81752ef4a565aba8627a4dc3c4113151
Abhinav Chawla <abhinav.chawla@ibm.com>
2020-10-07 20:22:59 -0400

    add basic emotions images

--- constants/livechatCDNLinks.js
+++ constants/livechatCDNLinks.js
@@ -1,13 +1,13 @@
 // TODO: Update URLs
 module.exports = {
   tentative:
-    "https://cdn.livechat-files.com/api/file/lc/tmp/attachments/12262749/a3a4311acac8c28e321e80df46721755/Screen%20Shot%202020-09-29%20at%2012.21.49%20PM.png",
+    "https://cdn.livechat-files.com/api/file/lc/tmp/attachments/12262749/e6bb07b4a9efc0572d06efcc94232372/Screen%20Shot%202020-10-07%20at%208.17.47%20PM.png",
   sadness:
-    "https://cdn.livechat-files.com/api/file/lc/tmp/attachments/12262749/a3a4311acac8c28e321e80df46721755/Screen%20Shot%202020-09-29%20at%2012.21.49%20PM.png",
+    "https://cdn.livechat-files.com/api/file/lc/tmp/attachments/12262749/3d20d3b87165d0162dd74d7799426316/Screen%20Shot%202020-10-07%20at%207.47.38%20PM.png",
   anger:
-    "https://cdn.livechat-files.com/api/file/lc/tmp/attachments/12262749/a3a4311acac8c28e321e80df46721755/Screen%20Shot%202020-09-29%20at%2012.21.49%20PM.png",
+    "https://cdn.livechat-files.com/api/file/lc/tmp/attachments/12262749/0f5029c060c68905a8e8d445d5b18dff/Screen%20Shot%202020-10-07%20at%207.48.43%20PM.png",
   joy:
-    "https://cdn.livechat-files.com/api/file/lc/tmp/attachments/12262749/a3a4311acac8c28e321e80df46721755/Screen%20Shot%202020-09-29%20at%2012.21.49%20PM.png",
+    "https://cdn.livechat-files.com/api/file/lc/tmp/attachments/12262749/3e2bdc37a5b1d871f35930ac057b050a/Screen%20Shot%202020-10-07%20at%207.48.22%20PM.png",
   zeroPoint:
     "https://cdn.livechat-files.com/api/file/lc/tmp/attachments/12262749/a3a4311acac8c28e321e80df46721755/Screen%20Shot%202020-09-29%20at%2012.21.49%20PM.png",
   onePoint:
